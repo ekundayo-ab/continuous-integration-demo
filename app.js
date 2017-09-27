9e9aa7e2e88f0e3e19f901483889afea6f2f12e3
Ekundayo Abiona <ekundayoabiona@gmail.com>
2017-09-27 13:23:54 +0100

    add calculator logic (add, subtract and multiply)

---
+++ app.js
@@ -0,0 +1,7 @@
+import Calculator from './helpers';
+
+const cl = new Calculator(9, 10);
+
+console.log(cl.add());
+console.log(cl.subtract());
+console.log(cl.multiply());
---
+++ helpers.js
@@ -0,0 +1,20 @@
+class Calculator {
+  constructor(val1, val2) {
+    this.val1 = val1;
+    this.val2 = val2;
+  }
+
+  add() {
+    return this.val1 + this.val2;
+  }
+
+  subtract() {
+    return this.val1 - this.val2;
+  }
+
+  multiply() {
+    return this.val1 * this.val2;
+  }
+}
+
+export default Calculator;
