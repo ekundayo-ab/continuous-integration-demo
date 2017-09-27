e51d04d78ade906c57acd6e06d503ad7c452fc43
Ekundayo Abiona <ekundayoabiona@gmail.com>
2017-09-27 15:02:13 +0100

    Include test for adding, subtracting and multiplying

---
+++ appTest.js
@@ -0,0 +1,17 @@
+/* eslint-env mocha */
+import assert from 'assert';
+import Calculator from './helpers';
+
+const Calc = new Calculator(5, 5);
+
+describe('Calculator processes', () => {
+  it('should return 10 when adding 5 and 5', () => {
+    assert.equal(10, Calc.add());
+  });
+  it('should return 0 when subtracting 5 from 5', () => {
+    assert.equal(0, Calc.subtract());
+  });
+  it('should return 25 when multiplying 5 by 5', () => {
+    assert.equal(25, Calc.multiply());
+  });
+});
